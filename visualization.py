c4612dd6546e9dac73d68b98254fed218bf8d726
Alec Ho <alecho@alecs-mbp.attlocal.net>
2018-02-18 17:42:22 -0800

    sorted data into lists for most reviewed and best movies

--- visualization.py
+++ visualization.py
@@ -8,28 +8,39 @@ def main():
 	# Y_train = np.loadtxt('./data/train.txt').astype(int)
  #    Y_test = np.loadtxt('./data/test.txt').astype(int)
     data = np.loadtxt('./data/data.txt').astype(int)
-    # movies = np.genfromtxt('./data/movies.txt',dtype='str')
     movie_file = codecs.open('./data/movies.txt', mode='r', encoding='windows-1252')
     movie_names = {}
-    ratings = {}
+    genres = {}
     for line in movie_file:
     	movie_info = line.split()
     	movie_names[int(movie_info[0])] = " ".join(movie_info[1:-19])
-    	ratings[int(movie_info[0])] = list(map(int, movie_info[-19:]))
+    	genres[int(movie_info[0])] = list(map(int, movie_info[-19:]))
 
     M = max(data[:,0]).astype(int) # users
     N = max(data[:,1]).astype(int) # movies
 
-    # All movies
-    frequencies = Counter(data[:,1])
+    # 1. All movies
+    frequencies = Counter(data[:,1]) # how often the movies are reviewed
+
 
     # 2. Ten most popular movies
     most_reviewed = frequencies.most_common(10)
     pop_movie_IDs = [x[0] for x in most_reviewed]
-    print(pop_movie_IDs)
+    pop_movie_names = [movie_names[ID] for ID in pop_movie_IDs]
+    pop_movie_genres = [genres[ID] for ID in pop_movie_IDs]
+    print("Most Reviewed Movies: ", pop_movie_names)
 
     # 3. Top ten best movies
-    # best_reviewed = 
+    avg_ratings = {}
+    for data_tuple in data:
+    	key = data_tuple[1]
+    	avg_ratings[key] = avg_ratings.get(key, 0) + data_tuple[2]/frequencies[key]
+    best_reviewed = dict(Counter(avg_ratings).most_common(10))
+    best_reviewed_names = [movie_names[ID] for ID in best_reviewed]
+    best_reviewed_genres = [genres[ID] for ID in best_reviewed]
+    print("Best Movies: ", best_reviewed_names)
+
+    # 4. Three genres of your choice
 
 if __name__ == "__main__":
     main()
