98a863ade73d7ab36489797c02482b540460e952
Alec Ho <alecho@alecs-mbp.attlocal.net>
2018-02-19 23:55:22 -0800

    made fancy visualization into a function

--- visualization.py
+++ visualization.py
@@ -45,7 +45,7 @@ def categorize(genre_dict, movieIDs):
 
     Input: 
         genre_dict: dictionary of the lists of genre specifcations
-        movieIDs: the IDs of the movies we are considering for the tally
+        movieIDs: the IDs of the movies we are considering
 
     Output: 
         a list of coordinates categorizing how serious(+)/escapist(-) and 
@@ -80,6 +80,88 @@ def categorize(genre_dict, movieIDs):
 
     return coordinates
 
+def fancy_plot(genre_dict, movieIDs, movieNames, title):
+    '''
+    This function plots the rating count given in a bar graph
+
+    Input: 
+        genre_dict: dictionary of the lists of genre specifcations
+        movieIDs: the IDs of the movies we are considering
+        movieNames: the names of the movies we are considering
+        title: what the title of the plot should be
+    Output: 
+        shows the plot
+        saves the plot under the title given 
+    '''
+
+    # Fancy visualization
+    coordinates = categorize(genre_dict, movieIDs)
+    X = [x[0] for x in coordinates]
+    Y = [x[1] for x in coordinates]
+    
+    fig = pl.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(X, Y,'ko',markersize=8)
+    lim = max([max(X), max(Y), abs(min(X)), abs(min(Y))])
+
+    xmin, xmax = (-lim-0.5,lim+0.5)
+    ymin, ymax = (-lim-0.5,lim+0.5)
+    ax.set_xlim([-lim-0.5,lim+0.5])
+    ax.set_ylim([-lim-0.5,lim+0.5])
+
+    # plot movie names
+    for label, xpt, ypt in zip(movieNames, X, Y):
+        ax.text(xpt-0.1, ypt+0.1, label)
+
+    # plot classifications
+    ax.text(-0.4, ymax+0.5, 'Serious')
+    ax.text(-0.4, ymin-0.5, 'Escapist')
+    ax.text(xmax-0.6, 0.5, 'Adrenaline-Rush')
+    ax.text(xmin-0.4, 0.5, 'Light-Hearted')
+
+    # removing the default axis on all sides:
+    for side in ['bottom','right','top','left']:
+        ax.spines[side].set_visible(False)
+ 
+    # removing the axis ticks
+    pl.xticks([]) # labels 
+    pl.yticks([])
+    ax.xaxis.set_ticks_position('none') # tick markers
+    ax.yaxis.set_ticks_position('none')
+
+    # get width and height of axes object to compute 
+    # matching arrowhead length and width
+    dps = fig.dpi_scale_trans.inverted()
+    bbox = ax.get_window_extent().transformed(dps)
+    width, height = bbox.width, bbox.height
+ 
+    # manual arrowhead width and length
+    hw = 1./20.*(ymax-ymin) 
+    hl = 1./20.*(xmax-xmin)
+    lw = 1. # axis line width
+    ohg = 0.3 # arrow overhang
+ 
+    # compute matching arrowhead length and width
+    yhw = hw/(ymax-ymin)*(xmax-xmin)* height/width 
+    yhl = hl/(xmax-xmin)*(ymax-ymin)* width/height
+ 
+    # draw x and y axis
+    ax.arrow(0, 0, xmax, 0, fc='k', ec='k', lw = lw, 
+         head_width=hw, head_length=hl, overhang = ohg, 
+         length_includes_head= True, clip_on = False) 
+    ax.arrow(0, 0, 0, ymax, fc='k', ec='k', lw = lw, 
+         head_width=yhw, head_length=yhl, overhang = ohg, 
+         length_includes_head= True, clip_on = False) 
+    ax.arrow(0, 0, xmin, 0, fc='k', ec='k', lw = lw, 
+         head_width=yhw, head_length=yhl, overhang = ohg, 
+         length_includes_head= True, clip_on = False) 
+    ax.arrow(0, 0, 0, ymin, fc='k', ec='k', lw = lw, 
+         head_width=yhw, head_length=yhl, overhang = ohg, 
+         length_includes_head= True, clip_on = False) 
+
+    pl.savefig(title)
+    pl.show()
+
 def get_rating_count(data, movieIDs):
     '''
     This function gets the rating count given the data we read in and some
@@ -102,6 +184,7 @@ def get_rating_count(data, movieIDs):
             elif rating[2] == 5: r5 += 1
 
     return [r1, r2, r3, r4, r5]
+
 
 def main():
     # Y_train = np.loadtxt('./data/train.txt').astype(int)
@@ -164,72 +247,8 @@ def main():
     pop_movie_genres = [genres[ID] for ID in pop_movie_IDs]
     print("Most Reviewed Movies: ", pop_movie_names)
 
-    # Fancy visualization
-    pop_coordinates = categorize(genres, pop_movie_IDs)
-    pop_X = [x[0] for x in pop_coordinates]
-    pop_Y = [x[1] for x in pop_coordinates]
-    
-    fig = pl.figure()
-    ax = fig.add_subplot(111)
-    ax.plot(pop_X,pop_Y,'ko',markersize=8)
-    lim = max([max(pop_X), max(pop_Y), abs(min(pop_X)), abs(min(pop_Y))])
-
-    xmin, xmax = (-lim-0.5,lim+0.5)
-    ymin, ymax = (-lim-0.5,lim+0.5)
-    ax.set_xlim([-lim-0.5,lim+0.5])
-    ax.set_ylim([-lim-0.5,lim+0.5])
-
-    # plot movie names
-    for label, xpt, ypt in zip(pop_movie_names, pop_X, pop_Y):
-        ax.text(xpt-0.1, ypt+0.1, label)
-
-    # plot classifications
-    ax.text(-0.4, ymax+0.5, 'Serious')
-    ax.text(-0.4, ymin-0.5, 'Escapist')
-    ax.text(xmax-0.6, 0.5, 'Adrenaline-Rush')
-    ax.text(xmin-0.4, 0.5, 'Light-Hearted')
-
-    # removing the default axis on all sides:
-    for side in ['bottom','right','top','left']:
-        ax.spines[side].set_visible(False)
- 
-    # removing the axis ticks
-    pl.xticks([]) # labels 
-    pl.yticks([])
-    ax.xaxis.set_ticks_position('none') # tick markers
-    ax.yaxis.set_ticks_position('none')
-
-    # get width and height of axes object to compute 
-    # matching arrowhead length and width
-    dps = fig.dpi_scale_trans.inverted()
-    bbox = ax.get_window_extent().transformed(dps)
-    width, height = bbox.width, bbox.height
- 
-    # manual arrowhead width and length
-    hw = 1./20.*(ymax-ymin) 
-    hl = 1./20.*(xmax-xmin)
-    lw = 1. # axis line width
-    ohg = 0.3 # arrow overhang
- 
-    # compute matching arrowhead length and width
-    yhw = hw/(ymax-ymin)*(xmax-xmin)* height/width 
-    yhl = hl/(xmax-xmin)*(ymax-ymin)* width/height
- 
-    # draw x and y axis
-    ax.arrow(0, 0, xmax, 0, fc='k', ec='k', lw = lw, 
-         head_width=hw, head_length=hl, overhang = ohg, 
-         length_includes_head= True, clip_on = False) 
-    ax.arrow(0, 0, 0, ymax, fc='k', ec='k', lw = lw, 
-         head_width=yhw, head_length=yhl, overhang = ohg, 
-         length_includes_head= True, clip_on = False) 
-    ax.arrow(0, 0, xmin, 0, fc='k', ec='k', lw = lw, 
-         head_width=yhw, head_length=yhl, overhang = ohg, 
-         length_includes_head= True, clip_on = False) 
-    ax.arrow(0, 0, 0, ymin, fc='k', ec='k', lw = lw, 
-         head_width=yhw, head_length=yhl, overhang = ohg, 
-         length_includes_head= True, clip_on = False) 
-
-    pl.show()
+    # Fancy plot
+    fancy_plot(genres, pop_movie_IDs, pop_movie_names, 'Visualization of Ten Most Popular Movies')
 
     # Histogram
     pop_rating_count = get_rating_count(data, pop_movie_IDs)
