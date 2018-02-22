dc43a86e1934e66fea0258e443bd2e683d8742ab
anniewang1914 <Annie@Annies-MacBook-Pro.local>
2018-02-21 16:35:02 -0800

    Added an off the shelf attempt using the surprise package

--- hw5method.py
+++ hw5method.py
@@ -92,7 +92,7 @@ def bar_plot(rating_count, title):
 
 def main():
     Y_train = np.loadtxt('./data/train.txt').astype(int)
-    Y_test = np.loadtxt('./data/test.txt')  .astype(int)
+    Y_test = np.loadtxt('./data/test.txt').astype(int)
 
     M = max(max(Y_train[:,0]), max(Y_test[:,0])).astype(int) # users
     N = max(max(Y_train[:,1]), max(Y_test[:,1])).astype(int) # movies
---
+++ offTheShelf.py
@@ -0,0 +1,30 @@
+import numpy as np
+from surprise import Dataset, evaluate, KNNBasic, Reader
+
+
+# for data in Y_train: 
+
+reader = Reader(line_format='user item rating', sep='\t')
+Y_train = Dataset.load_from_file('./data/train.txt', reader = reader)
+Y_train = Y_train.build_full_trainset()
+
+Y_test = np.loadtxt('./data/test.txt').astype(str)
+uid = list(Y_test[:, 0])
+mid = list(Y_test[:, 1])
+rating = list(Y_test[:, 2])
+
+sim_options = {
+    'name': 'cosine',
+    'user_based': False
+}
+
+knn = KNNBasic(k = 20, sim_options=sim_options)
+knn.fit(Y_train)
+
+error = 0
+for index, user in enumerate(uid): 
+	prediction = knn.predict(user, mid[index])
+	if float(np.around(prediction.est)) != float(rating[index]):
+		error += 1
+testError = float(error)/len(uid)
+print(testError)
